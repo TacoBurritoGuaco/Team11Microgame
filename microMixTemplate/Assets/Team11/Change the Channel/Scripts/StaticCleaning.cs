b94a05148646ae3b58c7102d6c962de7cfd3d839
YuhuaXiao <xiaoyuhu@sheridancollege.ca>
2024-03-05 15:19:56 -0500

    Starting Script & Test pieces

---
+++ microMixTemplate/Assets/Team11/Change the Channel/Scripts/StaticCleaning.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace team11
+{
+    public class StaticCleaning : MonoBehaviour
+    {
+        // Start is called before the first frame update
+
+    }
+}
