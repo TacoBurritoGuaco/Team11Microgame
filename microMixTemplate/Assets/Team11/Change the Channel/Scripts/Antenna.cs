1e0e181c09abbc27428acea7e7e17bc2e3d0add1
TacoBurritoGuaco <65143885+TacoBurritoGuaco@users.noreply.github.com>
2024-03-06 13:40:44 -0500

    Added acceleration to the antenna movement to make it more engaging

--- microMixTemplate/Assets/Team11/Change the Channel/Scripts/Antenna.cs
+++ microMixTemplate/Assets/Team11/Change the Channel/Scripts/Antenna.cs
@@ -16,11 +16,13 @@ namespace team11
 
         public float angle; //The current angle of the antenna
         public float speed; //The speed at which this angle will change
+        public float acceleration; //The acceleration of speed
 
         void Start()
         {
             angle = 0; //The base angle at which the antenna starts in
-            speed = 0.5f; //the base speed of the antenna
+            speed = 0f; //the base speed of the antenna
+            acceleration = 0.01f; //the base acceleration value
 
             rb = GetComponent<Rigidbody>(); //get the rigibody component of the antenna
         }
@@ -31,6 +33,17 @@ namespace team11
         {
             direction = stick.normalized;
             //stick.normalized is a vector2 with the values of the arcade cabinet joystick
+
+            //If the player is moving the stick either left or right
+            if (direction == new Vector2(-1, 0) || direction == new Vector2(1, 0))
+            {
+                speed += acceleration; //Add acceleration to speed
+            }
+            else
+            {
+                speed = 0; //resets speed to minimum
+            }
+            speed = Mathf.Clamp(speed, 0, 2); //clamps the speed value
         }
 
         private void FixedUpdate()
@@ -44,10 +57,6 @@ namespace team11
             if (direction == new Vector2(1, 0))
             {
                 angle -= speed; //increases angle by 0.5f
-            }
-            else //If the player is neither moving the stick to the left or to the right
-            {
-                angle += 0; //adds nothing! leaving the angle in place
             }
             angle = Mathf.Clamp(angle, -75, 75); //clamps the angle to maximum and minimum values
             //(It cannot be greater than this, or less than this)
