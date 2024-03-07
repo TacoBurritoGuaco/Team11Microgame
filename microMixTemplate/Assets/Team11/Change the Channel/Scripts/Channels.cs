37080a9a5ce377f9ed5f3b3baee9a99221c56a62
TacoBurritoGuaco <65143885+TacoBurritoGuaco@users.noreply.github.com>
2024-03-07 17:59:23 -0500

    Fixed animator to switch between different texture animations Created script that changes between channels whenever you hit the TV at the correct spot Ben's animation is currently a placeholder texture, waiting on him to implement it in main or a different branch

---
+++ microMixTemplate/Assets/Team11/Change the Channel/Scripts/Channels.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+
+namespace team11
+{
+    public class Channels : MicrogameEvents
+    {
+        public int currentChannel; //the current channel that should be playing
+        public float lastClearTime;
+
+        public Animator TV; //the tv animator
+        private MeshRenderer mRenderer; //the object's mesh renderer
+        public List<Material> channels; //the channel materials that will switch around
+
+        void Start()
+        {
+            mRenderer = GetComponent<MeshRenderer>();
+            changeChannel();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //If the last clearTime is NOT equals to the current clear time 
+            if (!(lastClearTime == GameObject.Find("StaticScreen").GetComponent<StaticCleaning>().clearTimes)) {
+                changeChannel();
+            }
+        }
+        public void changeChannel()
+        {
+            //Changes the channel texture animation
+            currentChannel = (int)Random.Range(0, 3);
+            TV.SetInteger("change", currentChannel);
+
+            //Changes the channel texture
+            mRenderer.material = channels[currentChannel];
+
+            lastClearTime = GameObject.Find("StaticScreen").GetComponent<StaticCleaning>().clearTimes; //updates last clear time to match current clear time
+            //Effectively changing the material's current "Channel"
+        }
+    }
+}
