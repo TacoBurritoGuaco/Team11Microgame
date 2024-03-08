41517bd899933d126fa0da3642720f6576c93672
YuhuaXiao <xiaoyuhu@sheridancollege.ca>
2024-03-07 21:05:02 -0500

    Hand slap animation added back

--- microMixTemplate/Assets/Team11/Change the Channel/Scripts/HandSlap.cs
+++ microMixTemplate/Assets/Team11/Change the Channel/Scripts/HandSlap.cs
@@ -12,27 +12,24 @@ namespace team11
         //Reference the hand animator
         public Animator handAnimator;
 
-
+        // Refernce the static clean script
         public StaticCleaning cleaningReference;
 
-        void Update()
-        {
-        }
 
+        //When press the button, slap hand, then delay for TV shake and press check
         protected override void OnButton1Pressed(InputAction.CallbackContext context)
         {
             base.OnButton1Pressed(context);
-            Debug.Log("hand slap");
+ 
 
             if (button1.WasPressedThisFrame())
             {
                 handAnimator.Play("Hand Slap");
                 Invoke("tvShaking", 0.5f);
-             
-
             }
         }
 
+        //Reference the static clean script for tv shake and clear time add
         public void tvShaking()
         {
             cleaningReference.slapTV();
